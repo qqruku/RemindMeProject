4cc4014d93db3b95255b2bb2998a2fb083f46473
qqruku <na.galashev@gmail.com>
2016-12-07 16:44:19 +0300

    second commit

---
+++ app/src/main/java/com/galashev/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.galashev.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Galashev on 07.12.2016.
+ */
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
