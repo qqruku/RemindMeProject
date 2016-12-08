00cc87a6aaec5e08c0ef1bdb2e74fd25f5e91df6
qqruku <na.galashev@gmail.com>
2016-12-08 15:57:41 +0300

    create toolbar

--- app/src/main/java/com/galashev/remindme/MainActivity.java
+++ app/src/main/java/com/galashev/remindme/MainActivity.java
@@ -2,6 +2,8 @@ package com.galashev.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by Galashev on 07.12.2016.
@@ -9,8 +11,24 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity{
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
